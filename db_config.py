1680e418a609e5d7fc899f3060d6257e62e56927
YoelIbarra <yoelibarra98@gmail.com>
2022-03-25 13:50:13 -0300

    CREATE tabla producto

--- db_config.py
+++ db_config.py
@@ -2,6 +2,19 @@ import sqlite3
 
 conexion = sqlite3.connect('proyecto.db');
 
+#Tabla
+create_query =  """
+                CREATE TABLE IF NOT EXISTS producto(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    tipo varchar(50) NOT NULL,
+                    modelo varchar(50) NOT NULL,
+                    referencia varchar(255) NOT NULL,
+                    fecha_insert DATETIME DEFAULT CURRENT_TIMESTAMP
+                )
+                """
+cursor = conexion.cursor()
+cursor.execute(create_query)
+conexion.commit()
 
 
 
