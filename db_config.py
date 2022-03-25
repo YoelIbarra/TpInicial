2271992baf050ddbc0b9a8f0339fc636f0c551dd
YoelIbarra <yoelibarra98@gmail.com>
2022-03-25 13:53:13 -0300

    Update
    Paso los create a funciones individuales

--- db_config.py
+++ db_config.py
@@ -1,27 +1,24 @@
 import sqlite3
 
-conexion = sqlite3.connect('proyecto.db');
+def conexion_db():
+    conexion = sqlite3.connect('proyecto.db');
+    return conexion
+
 
 #Tabla
-create_query =  """
-                CREATE TABLE IF NOT EXISTS producto(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    tipo varchar(50) NOT NULL,
-                    modelo varchar(50) NOT NULL,
-                    referencia varchar(255) NOT NULL,
-                    fecha_insert DATETIME DEFAULT CURRENT_TIMESTAMP
-                )
-                """
-cursor = conexion.cursor()
-cursor.execute(create_query)
-conexion.commit()
+def create_tablas(conexion):
+    create_query =  """
+                    CREATE TABLE IF NOT EXISTS producto(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        tipo varchar(50) NOT NULL,
+                        modelo varchar(50) NOT NULL,
+                        referencia varchar(255) NOT NULL,
+                        fecha_insert DATETIME DEFAULT CURRENT_TIMESTAMP
+                    )
+                    """
+    cursor = conexion.cursor()
+    cursor.execute(create_query)
+    conexion.commit()
+    conexion.close()
 
-
-
-
-
-
-
-
-
-conexion.close()
+#if(conexion) conexion.close()
