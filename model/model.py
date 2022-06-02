3dba483eea830025855c2d106f1e80ec7449720a
Yoel Ibarra <yoelibarra98@gmail.com>
2022-06-02 18:36:55 -0300

    update y delete
    agrego update y delete

--- model/model.py
+++ model/model.py
@@ -46,7 +46,35 @@ for fila in select_products():
     print(fila.type)
 """
 
+def update_product(id, p_type, p_model, p_reference):
+    product = select_product_id(id)
+    if (p_type != ""):
+        product.type = p_type
+    if (p_model != ""):
+        product.model = p_model
+    if (p_reference != ""):
+        product.reference = p_reference
+    product.save()
 """
 Prueba de update
 
+print(select_product_id(1).type)
+update_product(1,"modificado","ads","ads")
+print(select_product_id(1).type)
 """
+
+def delete_product(id):
+    product = select_product_id(id)
+    product.delete_instance()
+"""
+Prueba delete
+
+print(select_product_id(1).type)
+delete_product(1)
+try:
+    if(select_product_id(1)):
+        print("esta")
+except:
+    print("no esta")   
+
+    """
