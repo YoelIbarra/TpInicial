92d5bc05c86bb9c5687ae66ae61f27c2fb580dbe
Yoel Ibarra <yoelibarra98@gmail.com>
2022-06-02 18:23:55 -0300

    Update select
    acá traigo los select, general y por id

--- model/model.py
+++ model/model.py
@@ -28,4 +28,25 @@ def insert_product(p_type,p_model,p_reference):
 
     product.save()
 
-insert_product('asd','asd2','asda3e')
+#Para porbar que anda el insert.
+#insert_product('asd2','asd2','asda3e')
+
+def select_products():
+    results = Product.select()
+    return results
+def select_product_id(id):
+    result = Product.select().where(Product.id == id).get()
+    return result
+"""
+Prueba de select de productos
+product = select_product_id(1)
+print(product.type)
+
+for fila in select_products():
+    print(fila.type)
+"""
+
+"""
+Prueba de update
+
+"""
