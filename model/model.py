e82b6f566cce03915eb363b7f2b7e443ffb337ef
Yoel Ibarra <yoelibarra98@gmail.com>
2022-06-02 15:44:34 -0300

    insert
    Acá va el insert sin el datetime porque no lo pude hacer andar

--- model/model.py
+++ model/model.py
@@ -1,4 +1,6 @@
+from math import prod
 from peewee import *
+import datetime as date
 
 database = SqliteDatabase('prueba.db')
 
@@ -11,8 +13,19 @@ class Product(BaseModel):
     type = CharField()
     model = CharField()
     reference = CharField()
-    insert = DateField()
+    #insert = DateTimeField()
 
 
 database.connect()
 database.create_tables([Product])
+
+def insert_product(p_type,p_model,p_reference):
+    product = Product()
+    product.type = p_type
+    product.model = p_model
+    product.reference = p_reference
+    #product.insert = date.datetime.now()
+
+    product.save()
+
+insert_product('asd','asd2','asda3e')
