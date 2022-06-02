89a0e1f1fc55f39898892eb219f21242ac0ad026
sebacairola <sebastiancairola@gmail.com>
2022-06-02 01:01:33 -0300

    Agrego clase validator

---
+++ utils/validator.py
@@ -0,0 +1,12 @@
+import re
+
+
+class Validator():
+
+    @staticmethod
+    def empty_field_validation(field):
+        return not len(field.strip())
+
+    @staticmethod
+    def has_no_numbers_validation(field):
+        return bool(re.match(re.compile("^[a-zA-Z]+$"), field))
