8d5ff54a45d61824279ed36b6dac9e7247e78471
sebacairola <sebastiancairola@gmail.com>
2022-06-05 18:28:41 -0300

    Quito comentarios

--- model/base_model.py
+++ model/base_model.py
@@ -1,45 +1,7 @@
-# from math import prod
 from peewee import Model
 from peewee import SqliteDatabase
-# import datetime as date
 
 
 class BaseModel(Model):
     class Meta:
         database = SqliteDatabase('prueba.db')
-
-#   Para porbar que anda el insert.
-#   nsert_product('asd2','asd2','asda3e')
-
-
-"""
-Prueba de select de productos
-product = select_product_id(1)
-print(product.type)
-
-for fila in select_products():
-    print(fila.type)
-"""
-
-
-"""
-Prueba de update
-
-print(select_product_id(1).type)
-update_product(1,"modificado","ads","ads")
-print(select_product_id(1).type)
-"""
-
-
-"""
-Prueba delete
-
-print(select_product_id(1).type)
-delete_product(1)
-try:
-    if(select_product_id(1)):
-        print("esta")
-except:
-    print("no esta")
-
-    """
