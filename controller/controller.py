4f5e8587c115f92bf7dd942b3823119122f69ff1
Yoel Ibarra <yoelibarra98@gmail.com>
2022-08-09 20:29:37 -0300

    Ejemplo uso logging
    trato de usar el logging

--- controller/controller.py
+++ controller/controller.py
@@ -1,9 +1,12 @@
 from view.view import View
 from model.class_product import Product
 import model.db_config
+import logging
 
 
 class Controller():
+
+    logging.basicConfig(filename='controller.log',level="DEBUG")
 
     def __init__(self):
         self.model = Product()
@@ -16,7 +19,12 @@ class Controller():
         return self.model.get_products()
 
     def insert_product(self, tipo, modelo, referencia):
-        self.model.insert_product(tipo, modelo, referencia)
+        try:
+            self.model.insert_product(tipo, modelo, referencia)
+            logging.info("Se inserto el producto de tipo %s y modelo %s",tipo,modelo)
+        except:
+            logging.error("no se inserto nada")
+
 
     def delete_product(self, product_id):
         self.model.delete_product(product_id)
